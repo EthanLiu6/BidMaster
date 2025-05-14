0c878aa2e4fbd68cd5bf30bb62be7ed0b160a12c
EthanLiu6 <liue2336@gmail.com>
2025-05-15 00:10:43 +0800

    清理models的内容，需要请重新下载模型即可

--- resources/models/down_models.py
+++ resources/models/down_models.py
@@ -3,5 +3,5 @@ from scripts import config
 
 # modelscope download --model 'Qwen/Qwen2-7b' --local_dir './'
 
-print(snapshot_download(model_id=config.model_name, cache_dir='./'))
+print(snapshot_download(model_id=config.model_name, cache_dir='./qwen/'))
 
